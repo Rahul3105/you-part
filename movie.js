6de08433abcf683c805d7502441d13b68ca74790
rahul rajput <rahulrajput7358@gmail.com>
2021-06-24 20:27:18 +0530

    assignment 2

--- movie.js
+++ movie.js
@@ -1,3 +1,4 @@
+let movieCont = document.getElementsByClassName("movieCont")[0];
 async function getData() {
   let movieName = document.getElementById("name").value;
   movieName = movieName.trim().replace(/ /g, "+");
@@ -8,15 +9,21 @@ async function getData() {
     let data = await res.json();
     appendData(data);
   } catch (err) {
-    console.log(err);
+    console.log("error found");
+    movieCont.innerHTML = `<div style="width:20%;height:0;padding-bottom:10%;position:relative;" class="not_found">
+                <img src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif" style="width: 100%;" alt="">
+                Internet not working
+                </div>`;
   } finally {
     console.log("promise completed");
   }
 }
-let movieCont = document.getElementsByClassName("movieCont")[0];
+
 function appendData(data) {
   if (data.Error == "Movie not found!") {
-    alert("Please check movie name and try again");
+    movieCont.innerHTML = `<div style="width:20%;height:0;padding-bottom:10%;position:relative;" class="not_found">
+                <img src="https://media.giphy.com/media/Q7cTwUgxNorSg23xS3/giphy.gif" style="width: 100%;" alt="">
+            </div>`;
   } else {
     movieCont.innerHTML = "";
     let movie = document.createElement("div");
@@ -25,8 +32,13 @@ function appendData(data) {
                 <img src=${data.Poster} alt="">
                 <div class="movieInfo">
                     <p class="title">${data.Title}</p>
+                    <hr>
                     <p class="releaseDate">Release Date : <span>${data.Released}</span></p>
+                    <hr>
                     <p class="rating"><i class="fas fa-star" style="color: rgb(255, 145, 0);"></i> ${data.imdbRating}</p>
+                    <hr>
+                    <p class= 'genre'>${data.Genre}</p>
+                    <hr>
                     <p class = 'story'> ${data.Plot}</p>
                 </div>
 
